fcb4cb06347794e6e28296a6574e657fff0c8d3c
liyihehai <liyi_hehai@sina.com>
2019-09-15 09:03:31 +0800

    '删除自动代码部分'

--- ac_backend/src/main/resources/templates/front/autoCode/codeModel/dao.ftl
+++
@@ -1,7 +0,0 @@
-package ${map.packPath!''};
-
-import com.nnte.framework.base.BaseDao;
-
-public interface ${map.classPri!''}Dao extends BaseDao {
-
-}
--- ac_business/src/main/java/com/nnte/ac_business/mapper/confdb/ProjectMainDao.java
+++
@@ -1,8 +0,0 @@
-package com.nnte.ac_business.mapper.confdb;
-
-
-import com.nnte.framework.base.BaseDao;
-
-public interface ProjectMainDao extends BaseDao {
-
-}
