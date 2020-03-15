9448bc48a8a485d8b8d5d6c1aaab3d79070852e8
liyihehai <liyi_hehai@sina.com>
2020-03-15 17:52:54 +0800

    新增模板文件对象

---
+++ kr_business/src/main/java/com/nnte/kr_business/entity/autoReport/TemplateItem.java
@@ -0,0 +1,22 @@
+package com.nnte.kr_business.entity.autoReport;
+
+public class TemplateItem {
+    private String fileName;
+    private String submitName;
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getSubmitName() {
+        return submitName;
+    }
+
+    public void setSubmitName(String submitName) {
+        this.submitName = submitName;
+    }
+}
