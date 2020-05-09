e87ce493209b1f38a4c85539aacf1f32910d4e5c
liyihehai <liyi_hehai@sina.com>
2020-05-09 16:27:05 +0800

    弹出框样式更改，登录加密更改

--- kr_static/kr_backend/js/login.js
+++ kr_static/kr_backend/js/login.js
@@ -1,6 +1,7 @@
 function LoginModle(){
     this.userName;
     this.password;
+    this.onSetPwsFinished=function(code,msg,username) {};
     this.gAjax=new AppJSGlobAjax();
     this.submitLoginInfo=function (userName,password){
         if(!userName){
@@ -37,6 +38,33 @@ function LoginModle(){
             this.gAjax.setLocalStorageItem("defaultUserName","");
         $("#loginForm_id").attr("action",url);
         $("#loginForm_id").submit();
+    }
+    this.submitSetPws=function(userName,password,onSetPwsFinished){
+        this.userName=userName;
+        this.password=password;
+        this.onSetPwsFinished=onSetPwsFinished;
+        var data = JSON.stringify({ userName: userName});
+        this.gAjax.AjaxApplicationJson("/main/priCheck",data,function (content) {
+            if (content){
+                if (content.code==0){
+                    loginModle.SetPws(content.tmpKey,loginModle.userName,loginModle.password);
+                }else
+                    loginModle.onSetPwsFinished(content.code,content.msg,loginModle.userName);
+            }else
+                loginModle.onSetPwsFinished(9999,"校验异常",loginModle.userName);
+        });
+    }
+    this.SetPws=function(tmpKey,name,pwd){
+        var srvPwd=pwd;
+        var secret = new SecretModel();
+        var aimPwd = secret.AES_ECB_encrypt(srvPwd, tmpKey);
+        var data = JSON.stringify({ opeCode: name,setAimPwd:aimPwd});
+        this.gAjax.AjaxApplicationJson("/operator/setPws",data,function (content) {
+            if (content){
+                loginModle.onSetPwsFinished(content.code,content.msg,loginModle.userName);
+            }else
+                loginModle.onSetPwsFinished(9999,"返回异常",loginModle.userName);
+        });
     }
 }
 var loginModle=new LoginModle();
